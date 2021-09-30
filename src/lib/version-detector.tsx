6c4cb3347a828656608212c840848712caa0d289
madp <dongpeng.ma@shopee.com>
2021-09-30 17:53:50 +0800

    refactor: change version data structure

--- src/lib/version-detector.tsx
+++ src/lib/version-detector.tsx
@@ -2,13 +2,12 @@ import * as React from 'react'
 import { Modal } from 'infrad'
 import { ExclamationCircleOutlined } from 'infra-design-icons'
 import { useState, useEffect } from 'react'
-import semver from 'semver'
 import 'infrad/dist/antd.css'
 import { ajax } from 'rxjs/ajax'
 import { catchError, interval as rxjsInterval, NEVER, switchMap} from 'rxjs'
 
 interface IVersionDetectorProps {
-  localVersion: Record<string, any>
+  localVersion: string
   cancelable?: boolean
   url?: string
   interval?: number
@@ -16,7 +15,7 @@ interface IVersionDetectorProps {
 
 function getVersionApi(url: string) {
   return ajax.getJSON<{
-    [key: string]: any
+    version: string
   }>(url, {
     'cache-control': 'no-cache'
   })
@@ -31,27 +30,14 @@ export function VersionDetector({
   const [detected, setDetected] = useState(false)
   useEffect(() => {
     if (!detected) {
-      const localVersions = Object.keys(localVersion)
-      if (localVersions.length === 0) {
-        console.error('there is no version in your localVersion')
-        return
-      }
       const s = rxjsInterval(interval * 1000).pipe(switchMap(() => {
         return getVersionApi(url).pipe(catchError((e) => {
           console.error(e)
           return NEVER
         }))
       })).subscribe(res => {
-        const remoteVersions = Object.keys(res)
-        if (remoteVersions.length === 0) {
-          console.error(`there is no version ${url}`)
-          return
-        }
-        remoteVersions.sort((v1, v2) => semver.compare(v2, v1))
-        localVersions.sort((v1, v2) => semver.compare(v2, v1))
-        const latestServerVersion = remoteVersions[0]
-        const latestLocalVersion = localVersions[0]
-        if (latestServerVersion !== latestLocalVersion) {
+        const remoteVersion = res.version
+        if (localVersion !== remoteVersion) {
           const content = 'Detect a new version, please refresh window.'
           const onOk = () => window.location.reload()
           if (cancelable) {
